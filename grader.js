aecb67827002329277282be2df5c63cd24f24c3b
Fulvio Pascotto <fulvio.pascotto@gmail.com>
2013-07-08 23:26:30 +0000

    HW3

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var sys = require('util');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://still-taiga-8224.herokuapp.com/index.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,9 +39,22 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var assertUrlExists = function(url) {
+    var instr = url.toString();
+    if(!instr) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
 };
+
+var cheerioHtmlFile = function(buffer) {
+    return cheerio.load(buffer);
+};
+
+var readFileToBuffer = function(file){
+	return fs.readFileSync(file);
+}
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
@@ -61,14 +77,34 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var check = function(buffer, checksfile){
+    var checkJson = checkHtmlFile(buffer, checksfile);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);	
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        .option('-u, --url <html_url>', 'Url to index.html', clone(assertUrlExists), URL_DEFAULT )
+	.parse(process.argv);
+
+    	if(program.url){
+		rest.get(program.url).on("complete", function(result){
+			if(result instanceof Error){
+				sys.puts("Error: "+ result.message);
+				this.retry(5000);
+			}else{
+				var buf = new Buffer(result);		
+				check(buf,program.checks);
+			}
+		});
+	}else{
+		check(readFileToBuffer(program.file), program.checks);
+	}
+
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
